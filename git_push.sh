38c3afde814b04a116d81494678124b53d0d8676
pachanga <fatin.d.cooper@students.coookman.edu>
2017-07-04 07:15:35 +0000

    fifth commit

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 echo "# second-run-aws" >> README.md
 git init
 git add .
-git commit -m "fourth commit"
+git commit -m "fifth commit"
 git remote add origin git@github.com:CooperProjects/second-run-aws.git
 git push -u origin master
